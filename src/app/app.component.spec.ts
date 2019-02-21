27801da0b0bbfdb538846f4f91b12240c5b3e349
benouaer <taleb.benouaer@hmcts.net>
2019-02-21 12:06:44 +0000

    rename the library to reflect content and use

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'jui-formly'`, () => {
+  it(`should have as title 'govuk-formly-templates'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('jui-formly');
+    expect(app.title).toEqual('govuk-formly-templates');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to jui-formly!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to govuk-formly-templates!');
   });
 });
