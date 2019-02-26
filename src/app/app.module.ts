1d374c554a60cdc4bef76e8d9266a253a95344db
benouaer <taleb.benouaer@hmcts.net>
2019-02-26 19:40:23 +0000

    Change library reference to point to dist folder rather than src

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {ReactiveFormsModule} from '@angular/forms';
-import {FormlyModule} from '@ngx-formly/core';
-import {GovukFormlyTemplatesModule} from '../../projects/govuk-formly-templates/src/lib/govuk-formly-templates.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormlyModule } from '@ngx-formly/core';
+import { GovukFormlyTemplatesModule } from 'govuk-formly-templates';
 
 @NgModule({
   declarations: [
